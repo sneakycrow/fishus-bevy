f7ccd75669bca35ccb8a193ff2729c917148880a
Zachary Corvidae <zach@sneakycrow.dev>
2025-05-03 12:31:38 -0700

    Add bevy example game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
+        .run();
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+/// Spawns people with names
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Zachary Corvidae".to_string())));
+}
+
+/// Timer for how often we greet
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+/// Says hello to all the person's
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, greet_people);
+    }
 }
